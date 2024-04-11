b12569586d949dbd0590e9d0fd9cbe8506861328
jasanimeet <135941841+jasanimeet@users.noreply.github.com>
2024-04-11 14:49:31 +0530

    app change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // project import
-import Routes from 'routes';
+// import Routes from 'routes';
 import ThemeCustomization from 'themes';
 import ScrollTop from 'components/ScrollTop';
 
@@ -8,7 +8,8 @@ import ScrollTop from 'components/ScrollTop';
 const App = () => (
   <ThemeCustomization>
     <ScrollTop>
-      <Routes />
+      {/* <Routes /> */}
+      hello hello hello hello hello hello hello hello hello hello hello hello hello
     </ScrollTop>
   </ThemeCustomization>
 );
